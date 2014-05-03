680ceef79ecf19a1a80be3c666b57f51c1eb194d
Branko Trifunovic <tbranko@gmail.com>
2014-05-03 13:46:58 +0200

    added favicon

--- BruParkingWebsite/urls.py
+++ BruParkingWebsite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from home.views import NotificationEmailView
 
 from django.contrib import admin
@@ -8,5 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^thank-you/$', TemplateView.as_view(template_name='thank_you.html')),
 	url(r'^i18n/', include('django.conf.urls.i18n')),
+	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 	url(r'^$', NotificationEmailView.as_view()),
 )
